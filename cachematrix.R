12ee4e2625acd31f53890bef23bc35076d94d7b2
abigail2105 <abigail2105@yahoo.com>
2014-07-24 11:48:20 +0800

    second attempt at assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,27 +1,33 @@
-
-makeCacheMatrix <- function(x) {
-  
-  invx <- solve(x)
-  
-  library(SOAR)
-  Store(invx)
+makeCacheMatrix <- function(x = matrix()) {
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinv <- function(solve) m <<- solve
+  getinv <- function() m
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
 }
 
 ## This function reads an existing matrix - x MUST be a matrix, then 
-## creates and caches the inverse of the matrix using the Store function in SOAR 
-## using the name invx for the inverse matrix
-
+## creates and caches the inverse of the matrix 
 
 cacheSolve <- function(x, ...) {
+  m <- x$getinv()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinv(m)
+  m
+}
+
   ## Returns a matrix that is the inverse of 'x'
-  ## looks for invx using the find function in SOAR 
-  ## If invx is not found I can't work out how to make a condition 
-  ## so that it can create the inverse, which would use inverse <- solve(x)
-  ## If it is, invx is called with the name inverse and printed
-      inverse <- x  
-  library(SOAR)
-  find("invx")
-      inverse <<- invx
-      
-  print(inverse)
+  ## if x has ben cached it returns the cached inverse
+  ## if there is no cache it calculates and caches the inverse     
 }
