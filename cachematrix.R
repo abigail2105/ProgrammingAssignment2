3d971232c796efeb35f38f4f6c5b1e1cf33b29ee
abigail2105 <abigail2105@yahoo.com>
2014-07-24 11:13:29 +0800

    "try to commit Assignment2"

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,27 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x) {
+  
+  invx <- solve(x)
+  
+  library(SOAR)
+  Store(invx)
 }
 
+## This function reads an existing matrix - x MUST be a matrix, then 
+## creates and caches the inverse of the matrix using the Store function in SOAR 
+## using the name invx for the inverse matrix
 
-## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Returns a matrix that is the inverse of 'x'
+  ## looks for invx using the find function in SOAR 
+  ## If invx is not found I can't work out how to make a condition 
+  ## so that it can create the inverse, which would use inverse <- solve(x)
+  ## If it is, invx is called with the name inverse and printed
+      inverse <- x  
+  library(SOAR)
+  find("invx")
+      inverse <<- invx
+      
+  print(inverse)
 }
